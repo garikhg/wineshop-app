5fa95983cf36418fea31ee3dc2316ee9b4fbf134
garikhg <garik.28h@gmail.com>
2024-07-10 21:35:40 +0400

    Add Apollo client and update next.config
    A new file was created initializing the Apollo client for graphql communications with authentication capabilities. The next.config.mjs file was also updated to include a plugin for transpiling certain node_modules, customizable environment variables, and other additional configurations.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const withPlugins = require( 'next-compose-plugins' );
+const withTM = require( 'next-transpile-modules' )( [ '@apollo/client', 'graphql' ] );
 
-export default nextConfig;
+module.exports = withPlugins( [ withTM ], {
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [ 'node_modules' ],
+    },
+    env: {
+        GRAPHQL_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+        JWT_SECRET: process.env.JWT_SECRET,
+    },
+    webpack: ( config ) => {
+        // Further custom configuration here
+        return config;
+    },
+    images: {
+        domains: [ process.env.WORDADS_DOMAIN ],
+    },
+
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/product-category/:slug*',
+    //             destination: '/product-category/[slug]',
+    //         },
+    //         {
+    //             source: '/my-account/:slug*',
+    //             destination: '/my-account/[slug]',
+    //         },
+    //     ];
+    // },
+} );
+
+// export default nextConfig;
