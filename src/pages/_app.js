c303ae0310f46d09e93f0b1477243a47c0a74a89
garikhg <garik.28h@gmail.com>
2024-07-10 22:33:27 +0400

    Add Marcellus and Open Sans fonts to the app
    Three new font imports were added for Marcellus, Marcellus-SC, and Open Sans, which will enhance the overall typography of the Wineshop application. This change improves the visual design aesthetics of the app.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,9 @@ import "@styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { ApolloProvider } from "@apollo/client";
 import client from "@utils/apollo/apollo-client";
+import "@fontsource/marcellus";
+import "@fontsource/marcellus-sc";
+import "@fontsource/open-sans";
 
 export default function App( { Component, pageProps } ) {
     return (
