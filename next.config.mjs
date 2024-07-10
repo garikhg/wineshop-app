8998952ddfbdd5ba9709ea1f0f26254f6883c3d2
garikhg <garik.28h@gmail.com>
2024-07-10 22:17:50 +0400

    Refactor Next.js configuration file
    The Next.js configuration has been refactored for the WineShop application. This includes moving the file to a more standard .js format from .mjs and adding additional configuration options for plugins, environments, Webpack, and image domains. The changes aim to improve the application's functionality and compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require( 'next-compose-plugins' );
-const withTM = require( 'next-transpile-modules' )( [ '@apollo/client', 'graphql' ] );
-
-module.exports = withPlugins( [ withTM ], {
+const nextConfig = {
     reactStrictMode: true,
-    sassOptions: {
-        includePaths: [ 'node_modules' ],
-    },
-    env: {
-        GRAPHQL_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-        JWT_SECRET: process.env.JWT_SECRET,
-    },
-    webpack: ( config ) => {
-        // Further custom configuration here
-        return config;
-    },
-    images: {
-        domains: [ process.env.WORDADS_DOMAIN ],
-    },
-
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: '/product-category/:slug*',
-    //             destination: '/product-category/[slug]',
-    //         },
-    //         {
-    //             source: '/my-account/:slug*',
-    //             destination: '/my-account/[slug]',
-    //         },
-    //     ];
-    // },
-} );
-
-// export default nextConfig;
+}
+export default nextConfig;
